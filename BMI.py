ed5a8498971c1cff567f60b34ba070e56ea5f1ef
Thhhhh55 <h24721109@gmail.com>
2026-05-20 18:16:56 +0800

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,15 @@
+
+
+def category_index(weight, height):
+    BMI = weight / (height **2)
+    if BMI < 18.5:
+        category="偏瘦"
+    elif BMI<=25:
+        category="正常"
+    elif BMI<=30:
+        category="偏胖"
+    else:
+        category="肥胖"
+    print(f"您的BMI分类为:{category}")
+    print(f"您的BMI指数为{BMI}")
+    return BMI
